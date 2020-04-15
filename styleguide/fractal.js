0fb2f42f11fe8c0f0fc1f12e37cf1e99c3c3d965
bcalou <bastien.calou@gmail.com>
2020-04-15 17:34:41 +0200

    bugfix for conflict between fractal and parcel

--- styleguide/fractal.js
+++ styleguide/fractal.js
@@ -28,4 +28,4 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
 /*
  * Tell the Fractal web preview plugin where to look for static assets.
  */
-fractal.web.set('static.path', path.join(__dirname, '../dist'));
+// fractal.web.set('static.path', path.join(__dirname, 'public'));
